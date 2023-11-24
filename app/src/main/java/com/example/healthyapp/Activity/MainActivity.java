97ee119fc5a029f78be9c5969a111a24ff015803
thadeus <thadeusnyariki94@gmail.com>
2023-11-24 21:43:15 +0300

    :construction: feat: category-list-backend

--- app/src/main/java/com/example/healthyapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/healthyapp/Activity/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.healthyapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.healthyapp.Adapter.CategoryAdapter;
+import com.example.healthyapp.Domain.CategoryDomain;
 import com.example.healthyapp.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+   private RecyclerView.Adapter catAdapter,bestDealAdapter;
+   private RecyclerView recyclerViewCat, recyclerViewBestDeal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initRecyclerViewCat();
+    }
+
+    private void initRecyclerViewCat() {
+        ArrayList<CategoryDomain> items=new ArrayList<>();
+        items.add(new CategoryDomain("cat1","Vegetable"));
+        items.add(new CategoryDomain("cat2","Fruits"));
+        items.add(new CategoryDomain("cat3","Dairy"));
+        items.add(new CategoryDomain("cat4","Drinks"));
+        items.add(new CategoryDomain("cat5","Grain"));
+
+        recyclerViewCat=findViewById(R.id.catView);
+        recyclerViewCat.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        catAdapter=new CategoryAdapter(this,items);
+        recyclerViewCat.setAdapter(catAdapter);
     }
 }
